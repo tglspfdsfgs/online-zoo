954974c9eba8eae61979f3ef5d0f9b82f99a3398
tglspfdsfgs <tglspfdsfgs@gmail.com>
2022-10-08 12:49:12 +0300

    feat: complete a script of interaction with burger menu

---
+++ online-zoo/assets/scripts/burger-menu.js
@@ -0,0 +1,31 @@
+const header = document.querySelector('.header');
+const btn = header.querySelector('.header__burger-btn');
+const blur = document.querySelector(".blurred-background");
+
+
+function toggleBurgerMenu (event) {
+	blur.classList.toggle("blurred-background--on");
+	if ( header.classList.toggle("header--opened") ) {
+		disableScroll();
+	} else {
+		enableScroll();
+	}
+}
+
+btn.onpointerup = toggleBurgerMenu;
+blur.onpointerup = toggleBurgerMenu;
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
